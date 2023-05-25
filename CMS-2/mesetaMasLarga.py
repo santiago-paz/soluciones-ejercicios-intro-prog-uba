e34aa323136f0f6ed46348ec40be70d15a311835
Santiago Paz <santiago.paz.1992@gmail.com>
2023-05-25 16:08:17 -0300

    Completar ejercicio 3

--- CMS-2/mesetaMasLarga.py
+++ CMS-2/mesetaMasLarga.py
@@ -5,7 +5,33 @@ from typing import List
 # Respetar esta sintaxis, ya que el CMS dirá que no pasó ningún test si usan otra notación.
 def mesetaMasLarga(l: List[int]) -> int :
   # Implementar esta funcion
-  return 0
+  maximaMeseta = 0
+
+  for x in range(0, len(l), 1):
+    meseta = detectarMeseta(x, l)
+    if meseta > maximaMeseta:
+      maximaMeseta = meseta
+
+  return maximaMeseta
+
+def detectarMeseta(i: int, l: List[int]) -> int:
+  meseta = 1
+
+  if (i == len(l) - 1): return 1
+  if (i == len(l) - 2):
+    if (l[i] == l[i + 1]):
+      return 2
+    else:
+      return 1
+
+  for x in range(i, (len(l) - 1), 1):
+    if (l[x] == l[x + 1]):
+      meseta = meseta + 1
+    else:
+      break
+
+  return meseta
+
 
 if __name__ == '__main__':
   x = input()
